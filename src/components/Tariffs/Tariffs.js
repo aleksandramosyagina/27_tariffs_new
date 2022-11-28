79319121a298d6618472073fa532abf3fff6f09b
aleksandramosyagina <a.mosyagina@list.ru>
2022-11-28 09:17:44 +0300

    added selectedCard

--- src/components/Tariffs/Tariffs.js
+++ src/components/Tariffs/Tariffs.js
@@ -3,10 +3,12 @@ import CSSModules from 'react-css-modules';
 import '../Tariffs/tariff.css';
 
 function Tariffs(props, isSelected) {
+    // const {isSelected}=props.isSelected;
     // let planClass = classNames(style.App, props.isSelected? "selected":"" )
-    const selectedCard = (isSelected? "selected" : " ")
+    const selectedCard = (isSelected? "selected":"");
     return (
-        <div className={[style.App, selectedCard].join(' ')} >
+        // <div className={[style.App, selectedCard].join(' ')} >
+              <div className={[style.App, selectedCard].join(' ')} >
             <div className={style.name_style} styleName={props.color}>{props.name}</div>
             <div className={style.price_style} styleName={props.title_color}>руб {props.price} /мес</div>
             <div className={style.speed_style}>до {props.speed} Мбит/сек</div>
