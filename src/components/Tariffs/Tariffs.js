5b5a30ad5a1e4eac2f5602588ed18d059350a04d
aleksandramosyagina <a.mosyagina@list.ru>
2022-12-04 00:07:07 +0300

    added selected card

--- src/components/Tariffs/Tariffs.js
+++ src/components/Tariffs/Tariffs.js
@@ -1,14 +1,21 @@
 import style from '../Tariffs/tariffs.module.css';
 import CSSModules from 'react-css-modules';
 import '../Tariffs/tariff.css';
+import {useState} from 'react';
+import classNames from 'classnames';
 
-function Tariffs(props, isSelected) {
+function Tariffs(props) {
     // const {isSelected}=props.isSelected;
     // let planClass = classNames(style.App, props.isSelected? "selected":"" )
-    const selectedCard = (isSelected? "selected":"");
+    const [selected, setSelected] = useState(false);
+    const handleChange = () => {
+        setSelected (!selected);
+    }
+    // const selectedCard = props.isSelected ? "selected" : "";
     return (
         // <div className={[style.App, selectedCard].join(' ')} >
-              <div className={[style.App, selectedCard].join(' ')} >
+            //   <div className={[style.App, selectedCard].join(' ')} >
+<div onClick = {handleChange} className={classNames(style.App, selected ? style.selected : "")}>
             <div className={style.name_style} styleName={props.color}>{props.name}</div>
             <div className={style.price_style} styleName={props.title_color}>руб {props.price} /мес</div>
             <div className={style.speed_style}>до {props.speed} Мбит/сек</div>
